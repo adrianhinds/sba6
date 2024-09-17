2c55dc07ff1e1f9a11dc0327a0870175c13b5def
Adrian Hinds <adrianhinds@adrians-mbp.mynetworksettings.com>
2024-09-16 20:52:50 -0400

    updated index.js

--- routes/post.js
+++ routes/post.js
@@ -3,37 +3,65 @@ import post from '../models/post.js'
 
 const router = Router();
 
+router.get(':id', async (req, res) => {
+    let collection = await db.collection("posts")
+    let query = { _id: new ObjectId(req.params.id) }
+    let result = await collection.findOne(query)
+    console.log(result)
+    if(!result) {
+        res.send('Not found').status(400)
+    } else {
+        res.send(result).status(200)
+    }
+})
 router.get('/', async (req, res) => {
     try{
-
+      const posts = await Posts.find({}).limit(10).select('title author');
+      res.json(posts).status(200);
     }
-    catch {
-
+    catch (error) {
+res.json(error).status(400)
     }
 });
 router.post('/', async (req, res) => {
     try {
+        let collection = await db.collectin("posts")
+        let newDocument = req.body
+        newDocument.date = new Date()
+        let result = await collection.insertOne(newDocument)
+        console.log(result)
+        res.send(result).status(201)
+    }
+        catch (error) {
+            res.send(error).status(400)
+                }
+            });
 
+router.patch('/:id', async (req, res) => {
+    try {
+       const query = { _id: new ObjectId(req.params.id)}
+       const updates = {
+        $push: { comments: req.body }
+       }
+       let collection = await db.collection("posts")
+       let Updatedpatch = await collection.findOneAndUpdate(query, updates)
+       res.send(Updatedpatch).status(200)
     }
     catch {
-
+           console.log(error)
+           res.send(error).status(400)
     }
 });
-
-router.patch('/', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
-
+       const query = { _id: new ObjectId(req.params.id) }
+       let collection = await db.collection("posts")
+       let result = await collection.deleteOne(query)
+       res.send(result).status(200)
     }
-    catch {
-
-    }
-});
-router.delete('/', async (req, res) => {
-    try {
-
-    }
-    catch {
-
+    catch (error) {
+console.log(error)
+res.send(error).status(400)
     }
 });
 
